cd22b514ec980d427bc1ab676756fb45269b6967
manish-gkv <manishkumarslmp@gmail.com>
2024-08-23 10:52:59 +0530

    diff added

--- SyncForge.cpp
+++ SyncForge.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include<vector>
 #include<string>
+#include <algorithm>
 #include <fstream>
 #include <sstream>
 #include "sha1.h"
@@ -108,6 +109,77 @@ private:
 		}
 		return data;
 	}
+	int largestCommonSubsequence(std::vector<std::string> &file1, std::vector<std::string> &file2, int m, int n, std::vector<std::vector<int>> &dp){
+
+		if(m==0 || n==0) return 0; //Base Case
+
+		if (dp[m][n] != -1) return dp[m][n]; // Already exists in the dp table
+		//match
+		if(file1[m-1] == file2[n-1]){
+			return dp[m][n] = 1 + largestCommonSubsequence(file1, file2, m-1, n-1, dp);
+		}
+
+		//not match
+		return dp[m][n] = std::max(largestCommonSubsequence(file1, file2, m-1, n, dp), largestCommonSubsequence(file1, file2, m, n-1, dp));
+	}
+	std::vector<std::string> findLCSfromDP(std::vector<std::string> &file2, int m, int n, std::vector<std::vector<int>> &dp){
+		std::vector<std::string> res;
+		int i=m;
+		for(int j = n; j > 0; j--){
+			if(dp[i][j] == dp[i][j-1]){
+				continue;
+			}
+			else{
+				res.push_back(file2[j-1]);
+				i--;
+			}
+		}
+
+		reverse(res.begin(), res.end());
+
+		return res;
+	}
+
+	std::vector<std::string> find_diff(std::vector<std::string> file1, std::vector<std::string> file2){
+		std::vector<std::string> res;
+		int m = file1.size();
+		int n = file2.size();
+
+		if(m < n) {
+			return {"inavlid"};
+		}
+
+		int i = 0, j = 0;
+
+		while(j < n){
+			if(file1[i] == file2[j]){
+				i++;
+				j++;
+			}else{
+			res.push_back(file1[i]);
+			i++;
+			}
+		}
+
+		while(i < m){
+			res.push_back(file1[i]);
+			i++;
+		}
+
+		return res;
+	}
+	commitData getCommitData(std::string comitHash){
+		std::ifstream file(objectsPath/comitHash, std::ios::binary);
+		if(file.is_open()){
+			std::string data;
+			std::getline(file, data);
+			file.close();
+			return commitData::deserialize(data);
+		}
+		else{
+			std::cerr << "Error in Opening of ComitHash File\n";
+		}
+	}
 
 public:
 	SyncForge(){
@@ -187,27 +259,111 @@ public:
 			currentCommitHash = data.parent;
 		}
 	}
+	void showCommitDiff(std::string comitHash){
+		commitData data = getCommitData(comitHash);
+		std::cerr << "Changes in the Last Commit are:-\n";
+		if(data.parent!=""){
+			commitData parentData = getCommitData(data.parent);
+			for(auto file : data.files){
+				std::cerr << "\nFile Name : "<<file.filePath<<"\n\n";
+				std::ifstream fs(objectsPath/file.hash);
+				std::vector<std::string> v1,v2;
+				if(fs.is_open()){
+					std::string line;
+					while(std::getline(fs, line)){
+						v2.push_back(line);
+					}
+					fs.close();
+				}
+				else{
+					std::cerr << "Error opening file!\n";
+				}
+				for(auto f:parentData.files){
+					if(f.filePath==file.filePath){
+						fs.open(objectsPath/f.hash);
+						if(fs.is_open()){
+							std::string line;
+							while(std::getline(fs, line)){
+								v1.push_back(line);
+							}
+							fs.close();
+						}
+						else{
+							std::cerr << "Error opening file!\n";
+						}
+						break;
+					}
+				}
+
+				int m=v1.size(), n=v2.size();
+				std::vector<std::vector<int>> dp(m+1, std::vector<int>(n+1));
+				for(int i=0; i<=m; i++) for(int j=0; j<=n; j++) dp[i][j]=-1;
+				int lcslen = largestCommonSubsequence(v1, v2, m, n, dp);
+				std::vector<std::string> lcs = findLCSfromDP(v2, m, n, dp);
+
+				int i=0, j=0, k=0;
+				while(j<lcs.size()){
+					while(v1[i]!=lcs[j]) std::cerr<<"\033[1;31m-- : "<<v1[i++]<<"\033[0m\n";
+					while(v2[k]!=lcs[j]) std::cerr<<"\033[1;32m++ : "<<v2[k++]<<"\033[0m\n";
+					std::cerr<<"   : "<<lcs[j++]<<"\n";
+					k++;
+					i++;
+				}
+				while(i<m) std::cerr<<"\033[1;31m-- : "<<v1[i++]<<"\033[0m\n";
+				while(k<n) std::cerr<<"\033[1;32m++ : "<<v2[k++]<<"\033[0m\n";
+			}
+		}
+		else{
+			for(auto file : data.files){
+				std::cerr << "\nFile Name : "<<file.filePath<<"\n\n";
+				std::ifstream fs(objectsPath/file.hash);
+				std::vector<std::string> v1,v2;
+				if(fs.is_open()){
+					std::string line;
+					while(std::getline(fs, line)){
+						std::cerr<<"\033[1;32m++ : "<<line<<"\033[0m\n";
+					}
+					fs.close();
+				}
+				else{
+					std::cerr << "Error opening file!\n";
+				}
+			}
+		}
+	}
 };
 
 int main(int argc, char* argv[]) {
-    SyncForge S;
+    SyncForge syncforge;
 
-    if (argc > 1) {
-        std::string command = argv[1];
+    if (argc < 2) {
+        std::cerr << "No command provided!\n";
+        return 1;
+    }
 
-        if (command == "add" && argc == 3) {
-            std::string fileToAdd = argv[2];
-            S.add(fileToAdd);
-        } else if (command == "commit" && argc == 3) {
-            std::string message = argv[2];
-            S.commit(message);
-        } else if (command == "log") {
-            S.log();
-        } else {
-            std::cerr << "Unknown command or incorrect arguments.\n";
-        }
-    } else {
-        std::cerr << "Please provide a command.\n";
+    std::string command = argv[1];
+
+    if (command == "init") {
+        syncforge = SyncForge();
+        std::cerr << "Initialized empty SyncForge repository\n";
+    } 
+    else if (command == "add" && argc == 3) {
+        std::string fileToAdd = argv[2];
+        syncforge.add(fileToAdd);
+    } 
+    else if (command == "commit" && argc == 4) {
+        std::string message = argv[3];
+        syncforge.commit(message);
+    } 
+    else if (command == "log") {
+        syncforge.log();
+    } 
+    else if (command == "diff" && argc == 3) {
+        std::string commitHash = argv[2];
+        syncforge.showCommitDiff(commitHash);
+    } 
+    else {
+        std::cerr << "Unknown command or wrong number of arguments\n";
     }
 
     return 0;
